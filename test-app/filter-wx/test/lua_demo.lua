05be56bd0c654dd91a648e49f33156886ed4f3b3
Popescu Tiberiu <tiberiu1204@gmail.com>
2026-01-29 15:16:51 +0200

    Modified lua_demo to hijack jit compiler; with current settings, a loop of 3 is needed for successful hijacking

--- test-app/filter-wx/test/lua_demo.lua
+++ test-app/filter-wx/test/lua_demo.lua
@@ -1,24 +1,8 @@
 local jit = require("jit")
 local bit = require("bit")
-
 jit.opt.start("hotloop=1")
 
-function marker(x)
-  -- constants that will appear in machine code
-  local a = 0xDEADBEEF
-  local b = 0xCAFEBABE
-  local c = 0xFEEDFACE
-
-  x = bit.bxor(x, a)
-  x = x + b
-  x = bit.band(x, c)
-  return x
+local result = 0
+for i = 1, 3 do
+  result = bit.bxor(i, 0xDEADC0DE)
 end
-
--- trigger JIT
-for i = 1, 200 do
-  marker(i)
-end
-
--- dummy print to keep LuaJIT from optimizing away
-print(marker(123))
